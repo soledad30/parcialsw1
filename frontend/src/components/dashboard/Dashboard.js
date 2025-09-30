27f678563ab98ded4fbc945d144679feb3e256c9
Soledad BV <104368230+soledad30@users.noreply.github.com>
2025-09-30 04:53:07 -0400

    Update Dashboard.js

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+
 // src/components/dashboard/Dashboard.js
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -182,5 +183,6 @@ const handleJoinProject = () => {
     </div>
   );
 };
+
 
 export default Dashboard;

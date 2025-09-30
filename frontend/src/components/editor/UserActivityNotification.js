daa45238e375079b90e4c89ad78868f7c17dc5df
Soledad BV <104368230+soledad30@users.noreply.github.com>
2025-09-30 04:54:55 -0400

    Update UserActivityNotification.js

--- frontend/src/components/editor/UserActivityNotification.js
+++ frontend/src/components/editor/UserActivityNotification.js
@@ -1,3 +1,4 @@
+
 // src/components/editor/UserActivityNotification.js
 import React, { useState, useEffect } from 'react';
 import './UserActivityNotification.css';
@@ -55,5 +56,6 @@ const UserActivityNotification = () => {
     </div>
   );
 };
+
 
 export default UserActivityNotification;
